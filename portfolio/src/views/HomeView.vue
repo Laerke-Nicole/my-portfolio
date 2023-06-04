2bfd9c50e5959bc91f0b986886d46765df974778
Lærke Nielsen <laenie01@easv365.dk>
2023-06-04 15:37:43 +0200

    Styling portfolio

--- portfolio/src/views/HomeView.vue
+++ portfolio/src/views/HomeView.vue
@@ -17,9 +17,9 @@
         </p>
       </div>
       
-      <!-- buttons to go straight to contact or portfolio -->
+      <!-- buttons to go straight to portfolio or portfolio -->
       <div class="frontpage-buttons">
-        <RouterLink to="/contact">
+        <RouterLink to="/portfolio">
           <button id="button1" class="button">Kontakt mig</button>
         </RouterLink>
       </div>
@@ -63,33 +63,45 @@
     </div>
 
 
-    <div class="portfolioViewBorder">
-            <div class="portfolio-item-container">
 
-                <!-- making a loop for all of my projects -->
-                <div v-for="item in state" :key="item" class="portfolio-item">
+    <div class="portfolio-item-container">
 
-                    <!-- container for projects -->
-                    <div class="portfolio-content" :class="item.id">
+        <!-- making a loop for all of my projects -->
+        <div v-for="item in state" :key="item" class="portfolio-item">
 
-                        <!-- adding the information i want to show -->
-                        <RouterLink :to="{ name: 'portfoliodetails', params:{id: item.id}}">
-                            <div class="zoomImg">
-                                <img :src="item.image" class="product-image">
-                            </div>
-                        </RouterLink>
+          <!-- container for projects -->
+          <div class="portfolio-content" :class="item.id">
 
-                        <h4 class="p-number" :class="item.number">{{ item.number }}</h4>
-                        <h1 class="p-category" :class="item.category">{{ item.category }}</h1>
-                        <h3>{{ item.title }}</h3>
-                        <p>{{ item.description }}</p>
-                        <RouterLink :to="{ name: 'portfoliodetails', params:{id: item.id}}">
-                            <button id="button1">Se mit arbejde</button>
-                        </RouterLink>
+            <!-- left portfolio -->
+            <div class="portfolioBorder1">
+              <div id="portfolioSection1">
+                  <div class="portfolioSection1Content">
+                    <RouterLink :to="{ name: 'portfoliodetails', params:{id: item.id}}">
+                      <div>
+                        <img :src="item.image" class="product-image" alt="portfolio-img">
+                      </div>   
+                    </RouterLink>
+                  </div>
+              </div>
+            </div>
+            
+            <!-- right portfolio -->
+            <div class="portfolioBorder2">
+                <div id="portfolioSection2">
+                    <div class="portfolioSection2Content">
+                      <h4 class="p-number" :class="item.number">{{ item.number }}</h4>
+                      <h1 class="p-category" :class="item.category">{{ item.category }}</h1>
+                      <h3 class="p-title">{{ item.title }}</h3>
+                      <p>{{ item.description }}</p>
+                      <RouterLink :to="{ name: 'portfoliodetails', params:{id: item.id}}">
+                          <button id="button1">Se mit arbejde</button>
+                      </RouterLink>
                     </div>
                 </div>
-            </div>
+            </div>   
+          </div>
         </div>
+    </div>
   </main>
 </template>
 
@@ -252,132 +264,115 @@ button {
 
 
 /* making container of projects */
-.portfolioViewBorder {
-    padding: 15px;
-    background-color: var(--white-headline);
-}
-
 .portfolio-item-container {
-    display: flex;
-    flex-direction: column;
-    flex-wrap: nowrap;
-    overflow: hidden;
-    background-color: var(--white-headline);
-    padding: 0 8%;
-    padding-top: 45px;
-    padding-bottom: 60px;
-    border: var(--dark-border);
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  overflow: hidden;
+  background-color: var(--background-color);
+  padding-bottom: 60px;
+  width: 100%;
 }
 
 .portfolio-item {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    border: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  border: 0;
+  background-color: var(--background-color);
+  width: 100%;
+}
+
+.portfolio-content {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 24px;
+  width: 100%;
+}
+
+.portfolioBorder1 {
+  width: 50%;
+  padding: 15px 15px 15px 15px;
+  background-color: var(--white-headline);
+}
+/* portfolio container end */
+
+
+/* left side portfolio */
+#portfolioSection1 {
+  background-color: var(--white-headline);
+  border: var(--dark-border);
+  height: 600px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.portfolioSection1Content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 45px 8%;
+}
+
+
+/* right side portfolio */
+.portfolioBorder2 {
+  width: 50%;
+  padding: 15px 15px 15px 15px;
+  background-color: var(--secondary-color);
+}
+
+#portfolioSection2 {
+  background-color: var(--secondary-color);
+  display: flex;
+  flex-direction: row;
+  border: var(--bright-border);
+  height: 600px;
+}
+
+#portfolioSection2 .portfolioSection2Content {
+  padding-left: 8%;
+  padding-right: 8%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
 }
 /* making container of projects end */
 
 
-/* styling position of portfolios */
-.product-two {
-    padding: 60px 0 0 400px;
-}
-
-.product-three {
-    padding: 60px 0 0 120px;
-}
-
-.product-four {
-    padding: 60px 0 0 560px;
-}
-
-.product-five {
-    padding: 60px 0 0 0;
-}
-/* styling position of portfolios end */
-
-
 /* stying image */
-.zoomImg {
-    overflow: hidden;
-    height: 350px;
-}
-
-.portfolio-item-container img {
-    height: 350px;
-    overflow: hidden;
-    transition: transform .5s ease;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-
-.portfolio-item-container img:hover {
-    transform: scale(1.2);
-    height: 350px;
+.portfolioSection1Content img {
+  max-height: 510px;
 }
 /* stying image end */
 
 
 /* styling portfolio text */
 .p-number {
-    padding-top: 6px;
+  color: var(--white-text);
+  line-height: 0.7;
+}
+
+.p-category {
+  color: var(--white-headline);
+}
+
+.p-title {
+  color: var(--white-text);
 }
 
 .portfolio-content p {
-    padding-top: 16px;
-    padding-bottom: 12px;
+  padding-top: 16px;
+  padding-bottom: 12px;
+  color: var(--white-text);
 }
 /* styling portfolio text end */
 
 
 /* responsive */
-@media only screen and (max-width: 1200px) {
-    .product-two {
-    padding: 60px 0 0 350px;
-    }
-}
-
-@media only screen and (max-width: 1100px) {
-    .product-two {
-    padding: 60px 0 0 250px;
-    }
-}
-
-@media only screen and (max-width: 1000px) {
-    .product-two {
-        padding-top: 100px;
-        padding-left: 0;
-    }
-
-    .product-three {
-        padding-top: 100px;
-        padding-left: 0;
-    }
-
-    .product-four {
-        padding-top: 100px;
-        padding-left: 0;
-    }
-    
-    .product-five {
-        padding-top: 100px;
-    }
-}
-
 @media only screen and (max-width: 900px) {
-    .portfolio-item-container {
-        padding-top: 0;
-    }
-
-    .product-one {
-        padding-top: 0;
-    }
-
-    .product-two {
-        padding-top: 50px;
-    }
-
     .product-image {
         max-inline-size: 100%;
         block-size: auto;
