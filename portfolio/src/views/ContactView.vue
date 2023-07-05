182183d9481174877c12350e2382b6827e32f910
Lærke Nielsen <laenie01@easv365.dk>
2023-07-05 15:00:33 +0200

    New contact form

--- portfolio/src/views/ContactView.vue
+++ portfolio/src/views/ContactView.vue
@@ -2,7 +2,7 @@
     <main class="contactPage">
 
         <!-- left contact -->
-        <div class="bright-frame">
+        <!-- <div class="bright-frame">
             <div class="bright-frame-inside">
                 <div class="bright-frame-insideContent">
                     <h2>Kom og sig hej</h2>
@@ -21,10 +21,29 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
         
         <!-- right contact -->
-        <div class="secondary-frame">
+        <div class="secondary-frame w-full big-screens">
+            <div class="secondary-frame-inside framing-contact">
+                <div class="primary-frame">
+                    <div class="primary-frame-inside">
+                        <div class="primary-frame-insideContent">
+                            <h2>Arbejdshenvendelser</h2>
+                            <p>Kontakt mig venligst på</p>
+                            <p>laenie01@easv365.dk</p>
+                            <p class="pt-2 pb-2">eller</p>
+                            <a href="https://www.linkedin.com/in/l%C3%A6rke-nicole-nielsen/" target="_blank"><p class="linkedin-link underline">Linkedin</p></a>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- contact on smaller screens -->
+        <div class="secondary-frame small-screen">
             <div class="secondary-frame-inside">
                 <div class="secondary-frame-insideContent">
                     <h2>Arbejdshenvendelser</h2>
@@ -32,10 +51,11 @@
                     <p>laenie01@easv365.dk</p>
                     <p class="pt-2 pb-2">eller</p>
                     <a href="https://www.linkedin.com/in/l%C3%A6rke-nicole-nielsen/" target="_blank"><p class="linkedin-link underline">Linkedin</p></a>
-                    
                 </div>
             </div>
         </div>
+        
+
     </main>
 </template>
 
@@ -51,19 +71,20 @@ onMounted(() => {
 /* contact container */
 .contactPage {
     display: flex;
+    flex-direction: column;
     padding-top: 96px;
 }
 
-.bright-frame {
+/* .bright-frame {
     width: 50%;
     padding: 15px 15px 15px 15px;
     background-color: var(--white-headline);
-}
+} */
 /* contact container end */
 
 
 /* left side contact */
-.bright-frame-inside {
+/* .bright-frame-inside {
     background-color: var(--white-headline);
     border: var(--dark-border);
     min-height: 475px;
@@ -142,26 +163,45 @@ textarea::placeholder {
     font-family: "condor", sans-serif;
 font-weight: 400;
 font-style: normal;
-}
+} */
 /* left side contact end */
 
 /* right side contact */
-.secondary-frame {
-    width: 50%;
-    padding: 15px 15px 15px 15px;
-    background-color: var(--secondary-color);
+.framing-contact{
+    padding: 60px 20%;
 }
 
-.secondary-frame-inside {
-    background-color: var(--secondary-color);
+.primary-frame-inside {
     display: flex;
     flex-direction: row;
     border: var(--bright-border);
-    min-height: 475px;
+}
+
+.primary-frame-inside .primary-frame-insideContent {
+    padding: 60px 8% 80px 8%;
+}
+
+.primary-frame-inside h2 {
+    color: var(--white-headline);
+    font-size: 40px;
+    font-style: normal;
+    font-weight: bold; 
+}
+
+#primary-frame-inside h3, p {
+    color: var(--white-text);  
+}
+
+.linkedin-link:hover {
+    color: var(--primary-hover);
+}
+
+.small-screen {
+    display: none;
 }
 
 .secondary-frame-inside .secondary-frame-insideContent {
-    padding: 60px 8% 60px 8%;
+    padding: 60px 8% 80px 8%;
 }
 
 .secondary-frame-inside h2 {
@@ -173,17 +213,14 @@ font-style: normal;
 
 #secondary-frame-inside h3, p {
     color: var(--white-text);  
-}
+} 
 
-.linkedin-link:hover {
-    color: var(--primary-hover);
-}
 /* right side contact end */
 
 
 /* responsive */
 @media only screen and (max-width: 950px) {
-    .contactPage {
+    /* .contactPage {
         flex-direction: column;
         width: 100%;
     }
@@ -201,9 +238,18 @@ font-style: normal;
 
     textarea {
         min-width: 100%;
+    } */
+
+    /* .primary-frame {
+        width: 100%;
+    } */
+
+    .big-screens{
+        display: none;
     }
 
-    .secondary-frame {
+    .small-screen {
+        display: block;
         width: 100%;
     }
 
@@ -213,7 +259,7 @@ font-style: normal;
 }
 
 @media only screen and (max-width: 460px) {
-    .bright-frame-insideContent {
+    /* .bright-frame-insideContent {
     padding: 60px 30px 60px 30px;
     }
 
@@ -227,7 +273,7 @@ font-style: normal;
 
     textarea {
         min-width: 100%;
-    }
+    } */
 
     .secondary-frame-inside .secondary-frame-insideContent {
         padding: 60px 30px 60px 30px;
